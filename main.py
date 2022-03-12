73155019b178bf19ebe0f65623109a12f8c4a1ec
HarshuXD <95332135+HarshuXD@users.noreply.github.com>
2022-03-12 19:52:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ async def start_handler(bot: Client, m: Message):
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                 InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
+                [InlineKeyboardButton("Developer - @HARSHU_XD", url="https://t.me/HARSHU_XD")],
+                [InlineKeyboardButton("Support", url="https://t.me/HARSHU_XD"),
+                 InlineKeyboardButton("Channel", url="https://t.me/STARTERMODS")],
                 [InlineKeyboardButton("Open Settings", callback_data="openSettings")],
                 [InlineKeyboardButton("Close", callback_data="closeMeh")]
             ]
@@ -327,7 +327,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
                 user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cb.message.chat.id)
                 if user.status == "kicked":
                     await cb.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [MASTER](https://t.me/HARSHU_XD).",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
@@ -363,7 +363,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         await cb.message.edit(
             text=Config.START_TEXT,
             parse_mode="Markdown",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("MASTER - @HARSHU_XD", url="https://t.me/HARSHU_XD"), InlineKeyboardButton("Support", url="https://t.me/HARSHU_XD")], [InlineKeyboardButton("Channel", url="https://t.me/STARTERMODS")]]),
             disable_web_page_preview=True
         )
     elif "showThumbnail" in cb.data:
